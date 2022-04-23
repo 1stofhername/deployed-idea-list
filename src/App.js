167428b8fe2c6d6781222ecbd151deeaabb94629
Alicia <arrowsrp@yahoo.com>
2022-04-23 16:37:05 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect (()=>{
+    fetch('https://json-server-heroku-hosting-2.herokuapp.com/notes')
+    .then(res=>res.json())
+    .then(data=>console.log(data));
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hi I use a JSON server on Heroku!</h1>
     </div>
   );
 }
