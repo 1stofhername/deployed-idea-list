c5a96a1ffffd6838cb2fd98d43ca82be9d1f44dd
Alicia <arrowsrp@yahoo.com>
2022-04-23 16:59:44 -0700

    adds create button'

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import logo from './logo.svg';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
 
+const [data, setData]= useState([]);
+
   useEffect (()=>{
     fetch('https://json-server-heroku-hosting-2.herokuapp.com/notes')
     .then(res=>res.json())
-    .then(data=>console.log(data));
-  });
+    .then(data=>setData(data));
+  },[]);
+
+  console.log(data)
+
+  function handleAddHello(){
+    fetch('https://json-server-heroku-hosting-2.herokuapp.com/notes', {
+      method: 'POST',
+      body: JSON.stringify({
+        userId: 1, 
+        title:'hello',
+        body:"hello",
+        tags:"nothing"
+      }),
+      headers: {
+        "Content-type": "application/json"
+      }
+  })
+  .then(res=> res.json())
+  .then(data=>console.log(data))
+}
+  // .then(data=>tasks.push(data));
 
   return (
     <div className="App">
      <h1>Hi I use a JSON server on Heroku!</h1>
+     <button onClick={()=>handleAddHello()}>Add "Hello"</button>
+     
     </div>
   );
 }
