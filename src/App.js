1a88d7a21cbdf0dc6da52548414a6a95ae2594a1
Alicia <arrowsrp@yahoo.com>
2022-04-24 19:39:28 -0700

    Adds Header.js and NoteContainer.j

--- src/App.js
+++ src/App.js
@@ -1,42 +1,12 @@
-import logo from './logo.svg';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import NoteContainer from "./NoteContainer";
 
 function App() {
-
-const [data, setData]= useState([]);
-
-  useEffect (()=>{
-    fetch('https://json-server-heroku-hosting-2.herokuapp.com/notes')
-    .then(res=>res.json())
-    .then(data=>setData(data));
-  },[]);
-
-  console.log(data)
-
-  function handleAddHello(){
-    fetch('https://json-server-heroku-hosting-2.herokuapp.com/notes', {
-      method: 'POST',
-      body: JSON.stringify({
-        userId: 1, 
-        title:'hello',
-        body:"hello",
-        tags:"nothing"
-      }),
-      headers: {
-        "Content-type": "application/json"
-      }
-  })
-  .then(res=> res.json())
-  .then(data=>console.log(data))
-}
-  // .then(data=>tasks.push(data));
-
   return (
-    <div className="App">
-     <h1>Hi I use a JSON server on Heroku!</h1>
-     <button onClick={()=>handleAddHello()}>Add "Hello"</button>
-     
+    <div className="app">
+      <Header />
+      {/* <NoteContainer /> */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="nav-bar">
+      <ul>
+        <li className="nav-item">
+          <h1 className="title">Vault</h1>
+          <h3 className="sub-title">For all your bright ideas...</h3>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
