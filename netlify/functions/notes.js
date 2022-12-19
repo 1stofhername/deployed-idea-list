8392cb8f6c5a73dbeac941f4748915bb79f87953
Alicia <arrowsrp@yahoo.com>
2022-12-19 14:48:25 -0800

    imports data to notes.js function.

--- netlify/functions/notes.js
+++ netlify/functions/notes.js
@@ -2,10 +2,9 @@ import data from '../../src/data/notes.json' assert {type: 'json'};
 
 
 exports.handler = async function (event, context) {
-    console.log(data);
-    
+
     return {
         statusCode:200,
-        body: JSON.stringify({ message:"Hello World" }),
+        body: JSON.stringify({ data }),
     };
 };
