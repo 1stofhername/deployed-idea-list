42c825f0a71b331da5df400520f342cbc49cc958
Alicia <arrowsrp@yahoo.com>
2022-12-19 20:28:40 -0800

    Adds notes-by-id function.

---
+++ netlify/functions/note-by-id.js
@@ -0,0 +1,18 @@
+const notes = require('../../src/data/notes.json');
+
+exports.handler = async({ queryStringParameters }) => {
+    const { id } = queryStringParameters;
+    const note = notes.find((n) => n.id === id);
+
+    if (!note) {
+        return {
+            statusCode:404,
+            body: 'Not Found'
+        }
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(note),
+    }
+}
