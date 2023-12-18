95b46449a912a4b9f24115a5d75ef5904bdb78c8
Alicia <arrowsrp@yahoo.com>
2023-12-18 14:46:11 -0800

    Adds error handling for get-notes function.

--- netlify/functions/get-notes.js
+++ netlify/functions/get-notes.js
@@ -2,8 +2,14 @@ const notes = require('../../src/data/notes.json');
 
 exports.handler = async function (event, context) {
 
+    if (notes){
     return {
         statusCode:200,
         body: JSON.stringify({ notes }),
-    };
+    };} else {
+        return {
+            statusCode:404,
+            body: `Server error. Please contact site admin.`
+        }
+    }
 };
